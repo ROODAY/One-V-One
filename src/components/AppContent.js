01356b4b12f446cd8c1692e925cf821a4f00cf90
Rudhra Raveendran <rudytheninja@gmail.com>
2019-04-03 17:44:21 +0800

    basic layout

---
+++ src/components/AppContent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+
+class AppContent extends Component {
+  render() {
+    return (
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="home" title="Upcoming">
+          Upcoming Battles
+        </Tab>
+        <Tab eventKey="profile" title="Live">
+          Live Battles
+        </Tab>
+        <Tab eventKey="contact" title="Archive">
+          Past Battles
+        </Tab>
+      </Tabs>
+    );
+  }
+}
+
+export default AppContent;
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
+class AppNavbar extends Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">One V. One</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Settings</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default AppNavbar;
