44659bd571f19b116b44c638c769db658c2f6db3
jinghu <jinghul@bu.edu>
2019-04-09 19:25:53 +0800

    error print

--- data/lyric_scraper.py
+++ data/lyric_scraper.py
@@ -37,6 +37,7 @@ def get_lyrics(artist, song_title):
         lyrics = lyrics.replace('<br>','').replace('</br>','').replace('</div>','').strip()
         
         return lyrics
-    except Exception:
+    except Exception as err:
+        print(err)
         print('---- Found none.')
         return None
