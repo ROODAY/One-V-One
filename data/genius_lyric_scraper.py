34f6c93da7b84e25ffd9e71f9db08e5cf00e86d2
jinghu <jinghul@bu.edu>
2019-04-10 18:58:35 +0800

    fix

--- data/genius_lyric_scraper.py
+++ data/genius_lyric_scraper.py
@@ -9,7 +9,7 @@ genius.skip_non_songs = False # Include hits thought to be non-songs (e.g. track
 genius.verbose = False # Turn off status messages
 
 def get_lyrics(song_title, artist):
-    try {
+    try:
         song = genius.search_song(song_title, artist)
         if song:
             print('---- GENIUS: OK.')
@@ -17,9 +17,9 @@ def get_lyrics(song_title, artist):
         else:
             print('---- GENIUS: No lyrics found for {} by {}.'.format(song_title, artist))
             return None
-    } except requests.exceptions.Timeout {
+    except requests.exceptions.Timeout:
         print('---- GENIUS: Timeout occured.')
         return None
-    } except Exception {
+    except:
         print('---- GENIUS: Exception occured.')
-    }
+        return None
