35daaf059505c063a660c7de7af229d9b28dbe43
jinghu <jinghul@bu.edu>
2019-04-10 18:57:20 +0800

    fix

--- data/genius_lyric_scraper.py
+++ data/genius_lyric_scraper.py
@@ -1,6 +1,7 @@
 # Use GENIUS lyrics + Beautiful Soup to extract lyrics
 import lyricsgenius
 import raw.config as config
+import requests
 
 genius = lyricsgenius.Genius(config.client_access_token)
 genius.remove_section_headers = True # Remove section headers (e.g. [Chorus]) from lyrics when searching
@@ -8,10 +9,17 @@ genius.skip_non_songs = False # Include hits thought to be non-songs (e.g. track
 genius.verbose = False # Turn off status messages
 
 def get_lyrics(song_title, artist):
-    song = genius.search_song(song_title, artist)
-    if song:
-        print('---- GENIUS: OK.')
-        return song.lyrics
-    else:
-        print('---- GENIUS: No lyrics found for {} by {}.'.format(song_title, artist))
+    try {
+        song = genius.search_song(song_title, artist)
+        if song:
+            print('---- GENIUS: OK.')
+            return song.lyrics
+        else:
+            print('---- GENIUS: No lyrics found for {} by {}.'.format(song_title, artist))
+            return None
+    } except requests.exceptions.Timeout {
+        print('---- GENIUS: Timeout occured.')
         return None
+    } except Exception {
+        print('---- GENIUS: Exception occured.')
+    }
