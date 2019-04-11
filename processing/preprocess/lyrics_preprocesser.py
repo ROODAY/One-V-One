109a7f6cfc67a13d0f199eb6d1e6af819154dbb2
jinghu <jinghul@bu.edu>
2019-04-11 22:44:13 +0800

    finished model

---
+++ processing/preprocess/lyrics_preprocesser.py
@@ -0,0 +1,96 @@
+import io
+import os
+import re
+import math
+import json
+import pickle
+import string
+import requests
+import numpy as np
+from itertools import product
+from inspect import getsourcefile
+from os.path import abspath, join, dirname
+
+# Use NLTK tools to preprocess text into tf-idf vectors
+import nltk
+nltk.download()
+
+# Multilingual Stopwords
+stop_words = set(nltk.corpus.stopwords.words('english') + nltk.corpus.stopwords.words('spanish'))
+
+# Porter Stemmer for Preprocessing
+porter = nltk.PorterStemmer()
+
+def rm_html_tags(str):
+    html_prog = re.compile(r'<[^>]+>', re.S)
+    return html_prog.sub('', str)
+
+def rm_html_escape_characters(str):
+    pattern_str = r'&quot;|&amp;|&lt;|&gt;|&nbsp;|&#34;|&#38;|&#60;|&#62;|&#160;|&#20284;|&#30524;|&#26684|&#43;|&#20540|&#23612;'
+    escape_characters_prog = re.compile(pattern_str, re.S)
+    return escape_characters_prog.sub('', str)
+
+def rm_at_user(str):
+    return re.sub(r'@[a-zA-Z_0-9]*', '', str)
+
+def rm_url(str):
+    return re.sub(r'http[s]?:[/+]?[a-zA-Z0-9_\.\/]*', '', str)
+
+def rm_repeat_chars(str):
+    return re.sub(r'(.)(\1){2,}', r'\1\1', str)
+
+def rm_hashtag_symbol(str):
+    return re.sub(r'#', '', str)
+
+def rm_time(str):
+    return re.sub(r'[0-9][0-9]:[0-9][0-9]', '', str)
+
+def rm_punctuation(current_tweet):
+    return re.sub(r'[^\w\s]','',current_tweet)
+
+def preprocess_text(str):
+    str = str.lower()
+    str = rm_url(str)
+    str = rm_at_user(str)
+    str = rm_repeat_chars(str)
+    str = rm_hashtag_symbol(str)
+    str = rm_time(str)
+
+    try:
+        str = rm_punctuation(str)
+        str = nltk.tokenize.word_tokenize(str)
+        str = [porter.stem(t) for t in str]
+    except:
+        print(str)
+        
+    return str
+
+def preprocess_data(x, output_file):
+    words_stat = {}  # record statistics of the df and tf for each word; Form: { word:[tf, df,index] }
+    lyrics_list = []
+
+    for i, line in enumerate(x):
+        postprocess_text = []
+        words = preprocess_text(line)
+
+        for word in words:
+            if word not in stop_words:
+                postprocess_text.append(word)
+                if word in words_stat.keys():
+                    words_stat[word][0] += 1
+                    if i != words_stat[word][2]:
+                        words_stat[word][1] += 1
+                        words_stat[word][2] = i
+                else:
+                    words_stat[word] = [1,1,i]
+        lyrics_list.append(' '.join(postprocess_text))
+
+    # save lyrics bag of words in array
+    with open(output_file, 'w') as f:
+        f.writelines([lyrics+"\n" for lyrics in lyrics_list])
+
+    return lyrics_list
+
+def preprocess():
+    # Load list of words + TF-IDF statistics
+    pass
---
+++ processing/train_models.py
@@ -0,0 +1,108 @@
+import os
+import copy
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib
+
+# Preprocessers
+import preprocess.lyrics_preprocesser as lp
+
+# Sklearn helpers
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+# Model
+from sklearn.kernel_ridge import KernelRidge
+
+data_dir = "../data/"
+trained_dir = '../data/trained'
+data_file = 'filtered_song_info.csv'
+
+print('Loading data...')
+data = pd.read_csv(os.path.join(data_dir, data_file))
+
+# Drop missing and unused data
+data = data.dropna(axis=0, how='any')
+
+# Get ground truth
+labels = copy.deepcopy(data.song_popularity)
+data.drop("song_popularity", axis=1)
+
+# Join lyrics as stemmed words list for each song
+postprocess_lyrics = None
+if not os.path.isfile(os.path.join(data_dir, 'lyrics_stats.txt')):
+    postprocess_lyrics = lp.preprocess_data(data['lyrics'], os.path.join(data_dir, 'lyrics_stats.txt'))
+else:
+    with open(os.path.join(data_dir, 'lyrics_stats.txt'), 'r') as f:
+        postprocess_lyrics = [line.rstrip() for line in f.readlines()]
+postprocess_lyrics = np.array(postprocess_lyrics)
+data.drop("lyrics", axis=1)
+
+# Save trained Feature Selector - TFIDF and CountVectorizer
+MAX_FEATURES = 5000
+
+# count_v = CountVectorizer()
+tfidf_v = TfidfVectorizer(
+    analyzer='word', 
+    sublinear_tf=True,
+    strip_accents='unicode',
+    ngram_range=(1, 1), # unigram vs. bigram
+    max_features=MAX_FEATURES
+)
+postprocess_lyrics = tfidf_v.fit_transform(postprocess_lyrics)
+
+with open(os.path.join(trained_dir, 'tf_idfv.pkl'), 'w') as f:
+    pickle.dump(tfidf_v, f)
+
+# Convert to numpy array and shuffle
+data = data.to_numpy()
+data = np.concatenate((data, postprocess_lyrics), axis=1)
+print('---- data shape: {}'.format(data.shape))
+
+# OPTIONAL additional feature selection...
+# f_selector = SelectPercentile(f_classif, percentile=40)
+# data = f_selector.fit_transform(data, labels)
+# print('---- data shape after: {}'.format(data.shape))
+
+# Start training
+print("Start training and predict...")
+classifier = KernelRidge(alpha=2.0, kernel='rbf')
+
+# Saving model trained on data
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.3, random_state=7)
+model = classifier.fit(data[X_train], labels[y_train])
+
+# Predict
+y_pred = model.predict(X_test)
+nMSE = mean_squared_error(data[X_test], y_pred) / np.mean(np.square(labels[y_test]))
+print("achieved nMSE of {}".format(nMSE))
+del X_train, X_test, y_train, y_test
+
+# Start Validation
+print("Starting 10-Fold validation...")
+kf = KFold(n_splits=10)
+nMSEs = []
+count = 0
+
+for train, test in kf.split(data):
+    # train
+    model = classifier.fit(data[train], labels[train])
+        
+    # predict
+    predicts = model.predict(data[test])
+
+    nMSE = mean_squared_error(data[test], predicts) / np.mean(np.square(labels[test]))
+    nMSEs.append(nMSE)
+
+    count += 1
+    print("Round %d/10 of nMSE is: %f" %(count, nMSE))
+    
+print('Average nMSE is %f' %(np.mean(nMSEs)))
+
+
+
