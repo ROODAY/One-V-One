5c47ad44332888e43160d6bba071f78d2577a526
jinghu <jinghul@bu.edu>
2019-04-14 20:53:39 +0800

    changes

--- processing/train_popularity_model.py
+++ processing/train_popularity_model.py
@@ -63,10 +63,10 @@ feats_union = FeatureUnion([
         # ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
         # ('feat_sel', SelectPercentile(f_classif, percentile=20))
     # ])),
-    # ('tfidf_feats', Pipeline([
-        # ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
+    ('tfidf_feats', Pipeline([
+        ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
         # ('feat_sel', SelectPercentile(f_classif, percentile=60))
-    # ])),
+    ])),
     ('info', FunctionTransformer(get_song_info, validate=False))
 ])
 
