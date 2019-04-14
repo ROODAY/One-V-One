8fa6dcbcc73dbc1b510e7c025eaacb1ef72787be
jinghu <jinghul@bu.edu>
2019-04-15 03:21:02 +0800

    changes

--- predict/train_genre_model.py
+++ predict/train_genre_model.py
@@ -22,6 +22,7 @@ from sklearn.metrics import classification_report, precision_score, recall_score
 # Keras
 from keras import models
 from keras import layers
+from keras import backend as K
 
 data_dir = "../data/"
 data_file = 'genre_info.csv'
@@ -67,11 +68,42 @@ model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 
+# Custom F1 metric @Paddy and @Kev1n91 on StackOverflow
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
 # Form the model and fit
 print('Fitting model....')
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=[f1])
 history = model.fit(X_train, y_train, epochs=20, batch_size=128)
 
 # Show results
