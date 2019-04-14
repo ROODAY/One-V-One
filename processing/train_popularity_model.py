f6a16842c35cd6d8117312c16528a66d3dcfa13b
jinghu <jinghul@bu.edu>
2019-04-14 20:48:00 +0800

    only lyrics

--- processing/train_popularity_model.py
+++ processing/train_popularity_model.py
@@ -59,15 +59,15 @@ def get_song_info(x):
 
 # count_v = CountVectorizer()
 feats_union = FeatureUnion([ 
-    ('count_feats', Pipeline([
-        ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
+    # ('count_feats', Pipeline([
+        # ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
         # ('feat_sel', SelectPercentile(f_classif, percentile=20))
-    ])),
-    # ('tfidf_feats', Pipeline([
-        # ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
-        # ('feat_sel', SelectPercentile(f_classif, percentile=60))
     # ])),
-    ('info', FunctionTransformer(get_song_info, validate=False))
+    ('tfidf_feats', Pipeline([
+        ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
+        # ('feat_sel', SelectPercentile(f_classif, percentile=60))
+    ])),
+    # ('info', FunctionTransformer(get_song_info, validate=False))
 ])
 
 data = feats_union.fit_transform(postprocess_lyrics, labels)
