f250bf0e038910cf7fe8236ee243261ff775b309
jinghu <jinghul@bu.edu>
2019-04-15 00:04:50 +0800

    changes

--- predict/train_popularity_model.py
+++ predict/train_popularity_model.py
@@ -58,13 +58,13 @@ def get_song_info(x):
 
 feats_union = FeatureUnion([ 
     ('count_feats', Pipeline([
-        ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=50))
+        ('count', CountVectorizer(analyzer="word", ngram_range=(1,2),strip_accents='unicode', max_features=MAX_FEATURES)),
+        ('feat_sel', SelectPercentile(f_classif, percentile=20))
     ])),
-    ('tfidf_feats', Pipeline([
-        ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=50))
-    ])),
+    # ('tfidf_feats', Pipeline([
+        # ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
+        # ('feat_sel', SelectPercentile(f_classif, percentile=50))
+    # ])),
     ('info', FunctionTransformer(get_song_info, validate=False))
 ])
 
@@ -103,24 +103,19 @@ with open(os.path.join('../data/raw/', 'results.txt'), 'w') as f:
 # Start Validation
 print("Starting 10-Fold validation...")
 nMSEs = []
+kf = KFold(n_splits=10)
 
 # Mimic KFold splits
-kf = KFold(n_splits=10)
-fold = 1
-for train, test in kf.split(labels):
-    # X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.3)
-    # model = regressor.fit(X_train, y_train)
-    model = regressor.fit(data[train], labels[train])
+for fold in range(10):
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.3)
+    model = regressor.fit(X_train, y_train)
 
     # Predict
-    # y_pred = model.predict(X_test)
-    y_pred = model.predict(data[test])
-    # nMSE = mean_squared_error(y_test, y_pred) / np.mean(np.square(y_test))
-    nMSE = mean_squared_error(labels[test], y_pred) / np.mean(np.square(labels[test]))
+    y_pred = model.predict(X_test)
+    nMSE = mean_squared_error(y_test, y_pred) / np.mean(np.square(y_test))
     nMSEs.append(nMSE)
 
     print("Round %d/10 of nMSE is: %f" %(fold+1, nMSE))
-    fold += 1
     
 print('Average nMSE is %f' %(np.mean(nMSEs)))
 
