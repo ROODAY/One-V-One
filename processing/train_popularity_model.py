60466edb4b653789c0993b447f4d2c680abfee21
jinghu <jinghul@bu.edu>
2019-04-14 20:38:11 +0800

    test

--- processing/train_popularity_model.py
+++ processing/train_popularity_model.py
@@ -21,9 +21,9 @@ from sklearn.pipeline import Pipeline
 
 # Model
 # from sklearn.SVM import SVR
-from sklearn.kernel_ridge import KernelRidge
+# from sklearn.kernel_ridge import KernelRidge
 # from sklearn.neural_network import MLPRegressor
-# import xgboost as xgb
+import xgboost as xgb
 
 data_dir = "../data"
 data_lyrics_dir = data_dir + "/lyrics"
@@ -52,28 +52,28 @@ data = data.drop("lyrics", axis=1)
 
 # Save trained Feature Selector - TFIDF and CountVectorizer
 print("Starting feature extraction...")
-MAX_FEATURES = 5000
+MAX_FEATURES = 10000
 
 def get_song_info(x):
     return data.values
 
 # count_v = CountVectorizer()
 feats_union = FeatureUnion([ 
-    # ('count_feats', Pipeline([
-        # ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
-        # ('feat_sel', SelectPercentile(f_classif, percentile=10))
-    # ])),
-    ('tfidf_feats', Pipeline([
-        ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=40))
+    ('count_feats', Pipeline([
+        ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
+        ('feat_sel', SelectPercentile(f_classif, percentile=20))
     ])),
+    # ('tfidf_feats', Pipeline([
+        # ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
+        # ('feat_sel', SelectPercentile(f_classif, percentile=60))
+    # ])),
     ('info', FunctionTransformer(get_song_info, validate=False))
 ])
 
 data = feats_union.fit_transform(postprocess_lyrics, labels)
 
-# with open(os.path.join(trained_dir, 'popularity_funion.pkl'), 'wb') as f:
-    # pickle.dump(feats_union, f)
+with open(os.path.join(trained_dir, 'popularity_funion.pkl'), 'wb') as f:
+    pickle.dump(feats_union, f)
 
 print('---- data shape: {}'.format(data.shape))
 
@@ -95,12 +95,12 @@ print("---- model achieved nMSE of {}".format(nMSE))
 
 # y_pred = y_pred.tolist()
 y_test = y_test.tolist()
-with open(os.path.join('../data/raw/', 'results.txt'), 'w') as f:
+with open(os.path.join('../data/results/', 'xgb_results.txt'), 'w') as f:
     f.write('------------TRUTH vs. PREDICTS------------\n')
     f.writelines(['{} {}\n'.format(y_test[i], y_pred[i]) for i in range(len(y_test))])
 
-# with open(os.path.join(trained_dir, 'popularity_model.pkl'), 'wb') as f:
-    # pickle.dump(model, f)
+with open(os.path.join(trained_dir, 'popularity_model.pkl'), 'wb') as f:
+    pickle.dump(model, f)
 
 # Start Validation
 print("Starting 10-Fold validation...")
