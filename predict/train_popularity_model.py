e3977bb6036e79700f63949880e1fe3e25171242
jinghu <jinghul@bu.edu>
2019-04-14 23:59:12 +0800

    changes

--- predict/train_popularity_model.py
+++ predict/train_popularity_model.py
@@ -59,11 +59,11 @@ def get_song_info(x):
 feats_union = FeatureUnion([ 
     ('count_feats', Pipeline([
         ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=10))
+        ('feat_sel', SelectPercentile(f_classif, percentile=50))
     ])),
     ('tfidf_feats', Pipeline([
         ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 1), max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=10))
+        ('feat_sel', SelectPercentile(f_classif, percentile=50))
     ])),
     ('info', FunctionTransformer(get_song_info, validate=False))
 ])
