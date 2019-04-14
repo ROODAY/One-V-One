7eaa85bf2b9d1bdede65f411de6407480a0498d2
jinghu <jinghul@bu.edu>
2019-04-14 19:43:09 +0800

    count 20p

--- processing/train_popularity_model.py
+++ processing/train_popularity_model.py
@@ -60,10 +60,10 @@ def get_song_info(x):
 
 # count_v = CountVectorizer()
 feats_union = FeatureUnion([ 
-    ('tfidf', Pipeline([
+    ('count_feats', Pipeline([
         ('count', CountVectorizer(analyzer="word", ngram_range=(1,1),strip_accents='unicode', max_features=MAX_FEATURES)),
         # ('tfidf_v', TfidfVectorizer(analyzer='word', sublinear_tf=True, strip_accents='unicode', ngram_range=(1, 2), max_features=MAX_FEATURES)),
-        ('feat_sel', SelectPercentile(f_classif, percentile=10))
+        ('feat_sel', SelectPercentile(f_classif, percentile=20))
     ])),
     ('info', FunctionTransformer(get_song_info, validate=False))
 ])
