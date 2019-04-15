ef8b4d9e69db169e8869b6a7df575ed4e73364af
Rudhra Raveendran <rudytheninja@gmail.com>
2019-04-15 21:56:45 +0800

    minor fix

--- server/classifierWrapper.py
+++ server/classifierWrapper.py
@@ -2,7 +2,10 @@ from predict_popularity import predict as P_Predict
 from predict_genre import predict as G_Predict
 
 def runPrediction(audioFileName, transcripts):
+  text = ''
+  if len(transcripts) > 0:
+    text = transcripts[0]['text'] or ''
   return {
-    'hotness': P_Predict(audioFileName, transcripts[0]['text'] or '').tolist()[0],
+    'hotness': P_Predict(audioFileName, text).tolist()[0],
     'genre': G_Predict(audioFileName)
   }
