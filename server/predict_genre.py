b904a04e3e3a01c4b02e4369806b7d92f5bec706
Rudhra Raveendran <rudytheninja@gmail.com>
2019-04-15 19:07:30 +0800

    get around lfs

--- server/predict_genre.py
+++ server/predict_genre.py
@@ -5,11 +5,27 @@ import numpy as np
 from preprocess import audio_preprocesser
 from keras.models import load_model
 from tools import f1
+import requests
 
-trained_dir = '../data/trained'
+trained_dir = 'models'
+if not os.path.exists(trained_dir):
+  os.makedirs(trained_dir)
+
 encoder_file = 'genre_encoder.pkl'
 scaler_file = 'genre_scaler.pkl'
 model_file = 'nn_genre_model.h5'
+
+encoder_url = 'https://firebasestorage.googleapis.com/v0/b/one-v-one.appspot.com/o/models%2Fgenre_encoder.pkl?alt=media&token=5c797493-4d03-407f-98b2-2638e9dc025a'
+scaler_url = 'https://firebasestorage.googleapis.com/v0/b/one-v-one.appspot.com/o/models%2Fgenre_scaler.pkl?alt=media&token=e7ddb504-102f-4635-8848-9461fed22579'
+model_url = 'https://firebasestorage.googleapis.com/v0/b/one-v-one.appspot.com/o/models%2Fnn_genre_model.h5?alt=media&token=f6fb54d2-16a0-4fbf-a01d-22f95dee5e94'
+
+encoder_data = requests.get(encoder_url)
+scaler_data = requests.get(scaler_url)
+model_data = requests.get(model_url)
+
+open(os.path.join(trained_dir, encoder_file), 'wb').write(encoder_data.content)
+open(os.path.join(trained_dir, scaler_file), 'wb').write(scaler_data.content)
+open(os.path.join(trained_dir, model_file), 'wb').write(model_data.content)
 
 GENRES = [
   "Blues", 
--- server/predict_popularity.py
+++ server/predict_popularity.py
@@ -2,10 +2,23 @@ import os
 import pickle
 import pandas
 from preprocess import lyrics_preprocesser, audio_preprocesser
+import requests
 
-trained_dir = '../data/trained'
+trained_dir = 'models'
+if not os.path.exists(trained_dir):
+  os.makedirs(trained_dir)
+
 model_file = 'xgb_popularity_model.pkl'
 funion_file = 'xgb_popularity_funion.pkl'
+
+model_url = 'https://firebasestorage.googleapis.com/v0/b/one-v-one.appspot.com/o/models%2Fxgb_popularity_model.pkl?alt=media&token=2f84c0ae-4797-4fbf-a1db-b1f1bb635867'
+funion_url = 'https://firebasestorage.googleapis.com/v0/b/one-v-one.appspot.com/o/models%2Fxgb_popularity_funion.pkl?alt=media&token=a6a9ac91-4791-43ee-8250-ebdb362d7710'
+
+model_data = requests.get(model_url)
+funion_data = requests.get(funion_url)
+
+open(os.path.join(trained_dir, model_file), 'wb').write(model_data.content)
+open(os.path.join(trained_dir, funion_file), 'wb').write(funion_data.content)
 
 with open(os.path.join(trained_dir, funion_file), 'rb') as f:
     funion = pickle.load(f)
