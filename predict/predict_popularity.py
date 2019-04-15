edfb3dc6b358faceb67dce1ec89cfa5d308d9223
jinghu <jinghul@bu.edu>
2019-04-15 13:57:22 +0800

    api done

--- predict/predict_popularity.py
+++ predict/predict_popularity.py
@@ -7,9 +7,11 @@ trained_dir = '../data/trained'
 model_file = 'popularity_model.pkl'
 funion_file = 'popularity_funion.pkl'
 
-model = pickle.load(os.path.join())
+model = pickle.load(os.path.join(trained_dir, model_file))
+funion = pickle.load(os.path.join(trained_dir, funion_file))
 
 def predict(audio_file, lyrics):
     audio_mode, key, tempo = audio_preprocesser.preprocess_for_popularity(audio_file)
-    lyrics = 
-    model.predict()
+    features = [audio_mode, key, tempo, lyrics]
+    X = funion.transform(features)
+    return model.predict(X)
