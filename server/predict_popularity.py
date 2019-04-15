9b1381f0109d6cebca9b2c96ae687a1e10d6838a
jinghu <jinghul@bu.edu>
2019-04-15 16:34:00 +0800

    transform

--- server/predict_popularity.py
+++ server/predict_popularity.py
@@ -12,8 +12,13 @@ with open(os.path.join(trained_dir, funion_file), 'rb') as f:
 with open(os.path.join(trained_dir, model_file), 'rb') as f:
     model = pickle.load(f)
 
+pitch_list = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
 def predict(audio_file, lyrics):
     audio_mode, key, tempo = audio_preprocesser.preprocess_for_popularity(audio_file)
-    features = [audio_mode, key, tempo, lyrics]
-    [x] = funion.transform(features)
+    audio_mode = 0 if audio_mode=='minor' else 1
+    key = pitch_list.index(key)
+
+    features = [[audio_mode, key, tempo, lyrics]]
+    x = funion.transform(features)
     return model.predict(x)[0]
